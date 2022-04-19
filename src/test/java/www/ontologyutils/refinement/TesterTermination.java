88a37a4c468487630dd58433283b096409d2b1c6
Nicolas Troquard <ntroquard@unibz.it>
2022-04-19 18:48:32 +0200

    updated tester termination

--- src/test/java/www/ontologyutils/refinement/TesterTermination.java
+++ src/test/java/www/ontologyutils/refinement/TesterTermination.java
@@ -1,5 +1,7 @@
 package www.ontologyutils.refinement;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.IntSummaryStatistics;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -8,25 +10,29 @@ import java.util.stream.IntStream;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import uk.ac.manchester.cs.owl.owlapi.OWLSubClassOfAxiomImpl;
 import www.ontologyutils.toolbox.SetUtils;
 import www.ontologyutils.toolbox.Utils;
+import www.ontologyutils.toolbox.Utils.ReasonerName;
 
 /**
  * @author nico
  * 
  *         Testing almost-sure termination for the problem of reaching TOP by
- *         iterated generalization from an arbitrary concept wrt a reference
- *         ontology.
+ *         iterated generalization from BOT.
  */
 public class TesterTermination extends TestCase {
 
-	private static final String OWL_FILE_PATH = "resources/a-and-b.owl";
+	private static final String OWL_FILE_PATH = "resources/goslim_pir.owl"; // "resources/Random/C300_R10_0.0001_0.0001_0.0001_82227.owl";
+																			// "resources/a-and-b.owl";
 
 	private final static OWLClassExpression TOP = OWLManager.getOWLDataFactory().getOWLThing();
+	private final static OWLClassExpression BOT = OWLManager.getOWLDataFactory().getOWLNothing();
 	private final int NUMBER_TESTS = 100;
 	private final boolean PROPER = false; // true to force each generalization step to find non-equivalent
 											// generalization
@@ -34,48 +40,65 @@ public class TesterTermination extends TestCase {
 	static OWLOntology ontology;
 	static Covers covers;
 	static RefinementOperator generalisation;
+	static OWLReasoner equivReasoner;
 
 	public TesterTermination(String testName) {
 		super(testName);
+		Utils.DEFAULT_REASONER = ReasonerName.OPENLLET;
 
 		ontology = Utils.newOntology(OWL_FILE_PATH);
 		covers = new Covers(ontology);
 		generalisation = new RefinementOperator(covers.getUpCoverOperator(), covers.getDownCoverOperator());
+
+		equivReasoner = Utils.getReasoner(ontology);
 	}
 
 	public static Test suite() {
 		return new TestSuite(TesterTermination.class);
 	}
 
+	public static boolean areEquivalent(OWLClassExpression c1, OWLClassExpression c2) {
+		OWLSubClassOfAxiomImpl lr = new OWLSubClassOfAxiomImpl(c1, c2, new HashSet<>());
+		OWLSubClassOfAxiomImpl rl = new OWLSubClassOfAxiomImpl(c2, c1, new HashSet<>());
+		return equivReasoner.isEntailed(lr) && equivReasoner.isEntailed(rl);
+	}
+
 	public void testApp() {
 
-		System.out.println("**** Ontology " + OWL_FILE_PATH);
-		ontology.axioms().forEach(ax -> System.out.println(Utils.prettyPrintAxiom(ax)));
-		System.out.println("**** Sub of ontology " + OWL_FILE_PATH);
-		Utils.getSubOfTBox(ontology).forEach(e -> System.out.println(Utils.pretty(e.toString())));
-
 		IntSummaryStatistics summary = new IntSummaryStatistics();
+
+		int[] stepsArray = new int[NUMBER_TESTS];
+
 		IntStream.range(1, NUMBER_TESTS + 1).forEach(i -> {
 			int steps = 0;
-			OWLClassExpression e = SetUtils.getRandom(Utils.getSubOfTBox(ontology));
+			OWLClassExpression e = BOT;
 			System.out.format("**** Test %d. Trying to reach TOP from %s.%n", i, Utils.pretty(e.toString()));
 			while (!e.equals(TOP)) {
+				System.out.print(".");
 				steps++;
 				Set<OWLClassExpression> gen;
 				if (PROPER) {
 					final OWLClassExpression ee = e;
-					gen = generalisation.refine(e).stream()
-							.filter(c -> !Utils.areEquivalent(ee, c, ontology) || c == TOP).collect(Collectors.toSet());
+					gen = generalisation.refine(e).stream().filter(c -> !areEquivalent(ee, c) || c == TOP)
+							.collect(Collectors.toSet());
 				} else {
 					gen = generalisation.refine(e);
 				}
 				e = SetUtils.getRandom(gen);
-				System.out.println(Utils.pretty(e.toString()));
+				// System.out.println(Utils.pretty(e.toString()));
 			}
 			System.out.format("Required %d steps.%n", steps);
 			summary.accept(steps);
+			stepsArray[i - 1] = steps;
 		});
-		System.out.format("Average %f. Min %d. Max %d.", summary.getAverage(), summary.getMin(), summary.getMax());
+		Arrays.sort(stepsArray);
+		double median;
+		if (stepsArray.length % 2 == 0)
+			median = ((double) stepsArray[stepsArray.length / 2] + (double) stepsArray[stepsArray.length / 2 - 1]) / 2;
+		else
+			median = (double) stepsArray[stepsArray.length / 2];
+		System.out.format("Average %f. Median %f. Min %d. Max %d.", summary.getAverage(), median, summary.getMin(),
+				summary.getMax());
 
 		assertTrue(true);
 	}
