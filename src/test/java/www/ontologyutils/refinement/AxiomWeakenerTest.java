f054891509b7471d2b20d49cc3115ce0b40f8575
Roland Bernard <rolli.bernard@gmail.com>
2023-04-18 15:29:24 +0200

    Added tests for axiom weakening and strengthening

---
+++ src/test/java/www/ontologyutils/refinement/AxiomWeakenerTest.java
@@ -0,0 +1,129 @@
+package www.ontologyutils.refinement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+import java.util.stream.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+import org.semanticweb.owlapi.model.*;
+
+import www.ontologyutils.toolbox.Ontology;
+
+public class AxiomWeakenerTest {
+    private static final String ONTOLOGY_IRI = "http://www.semanticweb.org/ontologies/dl2017_example#";
+
+    private final Ontology ontology;
+    private final AxiomWeakener axiomWeakener;
+
+    public AxiomWeakenerTest() {
+        final var path = RoleCoverTest.class.getResource("../catsandnumbers.owl").getFile();
+        ontology = Ontology.loadOntology(path);
+        axiomWeakener = new AxiomWeakener(ontology);
+    }
+
+    private static Stream<Arguments> expectedWeakening() {
+        final var df = Ontology.getDefaultDataFactory();
+        return Stream.of(
+                Arguments.of(
+                        Set.of(
+                                df.getOWLSubClassOfAxiom(df.getOWLThing(), df.getOWLClass(ONTOLOGY_IRI, "Number")),
+                                df.getOWLSubClassOfAxiom(df.getOWLThing(),
+                                        df.getOWLClass(ONTOLOGY_IRI, "AbstractObject")),
+                                df.getOWLSubClassOfAxiom(df.getOWLClass(ONTOLOGY_IRI, "PhysicalObject"),
+                                        df.getOWLClass(ONTOLOGY_IRI, "Number")),
+                                df.getOWLSubClassOfAxiom(
+                                        df.getOWLObjectAllValuesFrom(
+                                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasQuality"),
+                                                df.getOWLClass(ONTOLOGY_IRI, "Primeness")),
+                                        df.getOWLClass(ONTOLOGY_IRI, "Number")),
+                                df.getOWLSubClassOfAxiom(
+                                        df.getOWLObjectAllValuesFrom(
+                                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour"),
+                                                df.getOWLClass(ONTOLOGY_IRI, "White")),
+                                        df.getOWLClass(ONTOLOGY_IRI, "Number")),
+                                df.getOWLSubClassOfAxiom(
+                                        df.getOWLObjectAllValuesFrom(
+                                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour"),
+                                                df.getOWLClass(ONTOLOGY_IRI, "Black")),
+                                        df.getOWLClass(ONTOLOGY_IRI, "Number"))),
+                        df.getOWLSubClassOfAxiom(df.getOWLThing(), df.getOWLClass(ONTOLOGY_IRI, "Number"))),
+                Arguments.of(
+                        Set.of(
+                                df.getOWLSubClassOfAxiom(df.getOWLClass(ONTOLOGY_IRI, "Colour"),
+                                        df.getOWLClass(ONTOLOGY_IRI, "Cat")),
+                                df.getOWLSubClassOfAxiom(df.getOWLClass(ONTOLOGY_IRI, "GrayScale"),
+                                        df.getOWLClass(ONTOLOGY_IRI, "Cat")),
+                                df.getOWLSubClassOfAxiom(df.getOWLClass(ONTOLOGY_IRI, "Colour"),
+                                        df.getOWLClass(ONTOLOGY_IRI, "Pet"))),
+                        df.getOWLSubClassOfAxiom(df.getOWLClass(ONTOLOGY_IRI, "Colour"),
+                                df.getOWLClass(ONTOLOGY_IRI, "Cat"))),
+                Arguments.of(
+                        Set.of(
+                                df.getOWLClassAssertionAxiom(df.getOWLClass(ONTOLOGY_IRI, "Cat"),
+                                        df.getOWLNamedIndividual(ONTOLOGY_IRI, "Leo")),
+                                df.getOWLClassAssertionAxiom(df.getOWLClass(ONTOLOGY_IRI, "Pet"),
+                                        df.getOWLNamedIndividual(ONTOLOGY_IRI, "Leo"))),
+                        df.getOWLClassAssertionAxiom(df.getOWLClass(ONTOLOGY_IRI, "Cat"),
+                                df.getOWLNamedIndividual(ONTOLOGY_IRI, "Leo"))),
+                Arguments.of(
+                        Set.of(
+                                df.getOWLClassAssertionAxiom(
+                                        df.getOWLClass(ONTOLOGY_IRI, "Cat"),
+                                        df.getOWLNamedIndividual(ONTOLOGY_IRI, "Leo")),
+                                df.getOWLClassAssertionAxiom(
+                                        df.getOWLObjectSomeValuesFrom(
+                                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour"),
+                                                df.getOWLClass(ONTOLOGY_IRI, "Black")),
+                                        df.getOWLNamedIndividual(ONTOLOGY_IRI, "Leo")),
+                                df.getOWLClassAssertionAxiom(
+                                        df.getOWLObjectIntersectionOf(
+                                                df.getOWLClass(ONTOLOGY_IRI, "Cat"),
+                                                df.getOWLClass(ONTOLOGY_IRI, "PhysicalObject")),
+                                        df.getOWLNamedIndividual(ONTOLOGY_IRI, "Leo")),
+                                df.getOWLClassAssertionAxiom(
+                                        df.getOWLObjectIntersectionOf(
+                                                df.getOWLClass(ONTOLOGY_IRI, "Cat"),
+                                                df.getOWLObjectSomeValuesFrom(
+                                                        df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour"),
+                                                        df.getOWLClass(ONTOLOGY_IRI, "GrayScale"))),
+                                        df.getOWLNamedIndividual(ONTOLOGY_IRI, "Leo")),
+                                df.getOWLClassAssertionAxiom(
+                                        df.getOWLObjectIntersectionOf(
+                                                df.getOWLClass(ONTOLOGY_IRI, "Pet"),
+                                                df.getOWLObjectSomeValuesFrom(
+                                                        df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour"),
+                                                        df.getOWLClass(ONTOLOGY_IRI, "Black"))),
+                                        df.getOWLNamedIndividual(ONTOLOGY_IRI, "Leo")),
+                                df.getOWLClassAssertionAxiom(df.getOWLObjectIntersectionOf(
+                                        df.getOWLClass(ONTOLOGY_IRI, "Cat"),
+                                        df.getOWLObjectSomeValuesFrom(
+                                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour"),
+                                                df.getOWLClass(ONTOLOGY_IRI, "Black"))),
+                                        df.getOWLNamedIndividual(ONTOLOGY_IRI, "Leo"))),
+                        df.getOWLClassAssertionAxiom(
+                                df.getOWLObjectIntersectionOf(
+                                        df.getOWLClass(ONTOLOGY_IRI, "Cat"),
+                                        df.getOWLObjectSomeValuesFrom(
+                                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour"),
+                                                df.getOWLClass(ONTOLOGY_IRI, "Black"))),
+                                df.getOWLNamedIndividual(ONTOLOGY_IRI, "Leo"))));
+    }
+
+    @ParameterizedTest
+    @MethodSource("expectedWeakening")
+    public void weakenAxiom(final Set<OWLAxiom> expected, final OWLAxiom axiom) {
+        assertEquals(expected, axiomWeakener.weakerAxioms(axiom).collect(Collectors.toSet()));
+    }
+
+    @Test
+    public void allWeakerAxiomsAreEntailed() {
+        ontology.axioms(AxiomWeakener.SUPPORTED_AXIOM_TYPES).forEach(strongAxiom -> {
+            axiomWeakener.weakerAxioms(strongAxiom).forEach(weakAxiom -> {
+                assertTrue(ontology.isEntailed(weakAxiom));
+            });
+        });
+    }
+}
--- src/test/java/www/ontologyutils/refinement/RoleCoverTest.java
+++ src/test/java/www/ontologyutils/refinement/RoleCoverTest.java
@@ -13,13 +13,13 @@ import org.semanticweb.owlapi.model.*;
 import www.ontologyutils.toolbox.Ontology;
 
 public class RoleCoverTest {
-    private static final String ONTOLOGY_IRI = "http://www.semanticweb.org/ontologies/dl2017_example#";
+    private static final String ONTOLOGY_IRI = "http://www.semanticweb.org/roland/ontologies/2023/3/untitled/";
 
     private final Ontology ontology;
     private final Covers covers;
 
     public RoleCoverTest() {
-        final var path = CoverTest.class.getResource("../catsandnumbers.owl").getFile();
+        final var path = CoverTest.class.getResource("../sroiq-tests.owl").getFile();
         ontology = Ontology.loadOntology(path);
         covers = new Covers(ontology);
     }
@@ -29,42 +29,57 @@ public class RoleCoverTest {
         return Stream.of(
                 Arguments.of(
                         Set.of(
-                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasQuality"),
-                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour")),
-                        df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour")),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "fatherOf"),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "parentOf"),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "childOf").getInverseProperty()),
+                        df.getOWLObjectProperty(ONTOLOGY_IRI, "fatherOf")),
                 Arguments.of(
-                        Set.of(df.getOWLObjectProperty(ONTOLOGY_IRI, "hasQuality")),
-                        df.getOWLObjectProperty(ONTOLOGY_IRI, "hasQuality")),
+                        Set.of(
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "childOf"),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "parentOf").getInverseProperty(),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "ancestorOf").getInverseProperty()),
+                        df.getOWLObjectProperty(ONTOLOGY_IRI, "childOf")),
+                Arguments.of(
+                        Set.of(),
+                        df.getOWLObjectProperty(ONTOLOGY_IRI, "grandparentOf")),
                 Arguments.of(
                         Set.of(),
                         df.getOWLTopObjectProperty()),
                 Arguments.of(
                         Set.of(
-                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour")
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "fatherOf")
                                         .getInverseProperty(),
-                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour")),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "fatherOf"),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "motherOf")
+                                        .getInverseProperty(),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "motherOf")),
                         df.getOWLBottomObjectProperty()),
                 Arguments.of(
                         Set.of(
-                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasQuality")
-                                        .getInverseProperty(),
-                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour")
-                                        .getInverseProperty()),
-                        df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour")
-                                .getInverseProperty()),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "fatherOf").getInverseProperty(),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "parentOf").getInverseProperty(),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "childOf")),
+                        df.getOWLObjectProperty(ONTOLOGY_IRI, "fatherOf").getInverseProperty()),
                 Arguments.of(
-                        Set.of(df.getOWLObjectProperty(ONTOLOGY_IRI, "hasQuality")
-                                .getInverseProperty()),
-                        df.getOWLObjectProperty(ONTOLOGY_IRI, "hasQuality")
-                                .getInverseProperty()),
+                        Set.of(
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "childOf").getInverseProperty(),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "parentOf"),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "ancestorOf")),
+                        df.getOWLObjectProperty(ONTOLOGY_IRI, "childOf").getInverseProperty()),
+                Arguments.of(
+                        Set.of(),
+                        df.getOWLObjectProperty(ONTOLOGY_IRI, "grandparentOf").getInverseProperty()),
                 Arguments.of(
                         Set.of(),
                         df.getOWLTopObjectProperty().getInverseProperty()),
                 Arguments.of(
                         Set.of(
-                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour")
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "fatherOf")
                                         .getInverseProperty(),
-                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour")),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "fatherOf"),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "motherOf")
+                                        .getInverseProperty(),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "motherOf")),
                         df.getOWLBottomObjectProperty().getInverseProperty()));
     }
 
@@ -79,44 +94,49 @@ public class RoleCoverTest {
         final var df = Ontology.getDefaultDataFactory();
         return Stream.of(
                 Arguments.of(
-                        Set.of(df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour")),
-                        df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour")),
+                        Set.of(
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "fatherOf")),
+                        df.getOWLObjectProperty(ONTOLOGY_IRI, "fatherOf")),
                 Arguments.of(
                         Set.of(
-                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour"),
-                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasQuality")),
-                        df.getOWLObjectProperty(ONTOLOGY_IRI, "hasQuality")),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "childOf"),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "parentOf").getInverseProperty(),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "fatherOf").getInverseProperty(),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "motherOf").getInverseProperty()),
+                        df.getOWLObjectProperty(ONTOLOGY_IRI, "childOf")),
+                Arguments.of(
+                        Set.of(),
+                        df.getOWLObjectProperty(ONTOLOGY_IRI, "grandparentOf")),
                 Arguments.of(
                         Set.of(
-                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasQuality")
-                                        .getInverseProperty(),
-                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasQuality")),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "knows").getInverseProperty(),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "knows")),
                         df.getOWLTopObjectProperty()),
                 Arguments.of(
                         Set.of(),
                         df.getOWLBottomObjectProperty()),
                 Arguments.of(
-                        Set.of(df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour")
-                                .getInverseProperty()),
-                        df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour")
-                                .getInverseProperty()),
+                        Set.of(
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "fatherOf").getInverseProperty()),
+                        df.getOWLObjectProperty(ONTOLOGY_IRI, "fatherOf").getInverseProperty()),
                 Arguments.of(
                         Set.of(
-                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasColour")
-                                        .getInverseProperty(),
-                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasQuality")
-                                        .getInverseProperty()),
-                        df.getOWLObjectProperty(ONTOLOGY_IRI, "hasQuality")
-                                .getInverseProperty()),
-                Arguments.of(
-                        Set.of(
-                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasQuality"),
-                                df.getOWLObjectProperty(ONTOLOGY_IRI, "hasQuality")
-                                        .getInverseProperty()),
-                        df.getOWLTopObjectProperty().getInverseProperty()),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "childOf").getInverseProperty(),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "parentOf"),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "fatherOf"),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "motherOf")),
+                        df.getOWLObjectProperty(ONTOLOGY_IRI, "childOf").getInverseProperty()),
                 Arguments.of(
                         Set.of(),
-                        df.getOWLBottomObjectProperty().getInverseProperty()));
+                        df.getOWLObjectProperty(ONTOLOGY_IRI, "grandparentOf").getInverseProperty()),
+                Arguments.of(
+                        Set.of(
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "knows").getInverseProperty(),
+                                df.getOWLObjectProperty(ONTOLOGY_IRI, "knows")),
+                        df.getOWLTopObjectProperty()),
+                Arguments.of(
+                        Set.of(),
+                        df.getOWLBottomObjectProperty()));
     }
 
     @ParameterizedTest
