9c7872fbb1d4d17790dd5f095171e8af2c3b084b
Roland Bernard <rolli.bernard@gmail.com>
2023-04-26 22:08:29 +0200

    Added options to the random mcs repair to compute only some mcs

--- src/main/java/www/ontologyutils/repair/OntologyRepairRandomMcs.java
+++ src/main/java/www/ontologyutils/repair/OntologyRepairRandomMcs.java
@@ -1,7 +1,8 @@
 package www.ontologyutils.repair;
 
-import java.util.Collection;
+import java.util.*;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import org.semanticweb.owlapi.model.*;
 
@@ -13,8 +14,19 @@ import www.ontologyutils.toolbox.*;
  * of axioms in the input ontology.
  */
 public class OntologyRepairRandomMcs extends OntologyRepair {
-    public OntologyRepairRandomMcs(final Predicate<Ontology> isRepaired) {
+    public static enum McsComputationStrategy {
+        All_MCS, SOME_MCS, ONE_MCS,
+    }
+
+    private final McsComputationStrategy mcsComputation;
+
+    public OntologyRepairRandomMcs(final Predicate<Ontology> isRepaired, final McsComputationStrategy mcs) {
         super(isRepaired);
+        this.mcsComputation = mcs;
+    }
+
+    public OntologyRepairRandomMcs(final Predicate<Ontology> isRepaired) {
+        this(isRepaired, McsComputationStrategy.All_MCS);
     }
 
     /**
@@ -41,9 +53,30 @@ public class OntologyRepairRandomMcs extends OntologyRepair {
         return new OntologyRepairRandomMcs(o -> o.isSatisfiable(concept));
     }
 
+    /**
+     * Compute some or all minimal correction subsets of the given ontology
+     * depending on the parameters of this object.
+     *
+     * @param ontology
+     *            The ontology for which to compute the minimal correction subset
+     * @return A stream containing minimal correction subsets
+     */
+    public Stream<Set<OWLAxiom>> computeMcs(final Ontology ontology) {
+        switch (mcsComputation) {
+            case All_MCS:
+                return ontology.minimalCorrectionSubsets(isRepaired);
+            case SOME_MCS:
+                return ontology.someMinimalCorrectionSubsets(isRepaired);
+            case ONE_MCS:
+                return Stream.of(ontology.minimalCorrectionSubset(isRepaired));
+            default:
+                throw new IllegalArgumentException("Unimplemented maximal consistent subset computation method.");
+        }
+    }
+
     @Override
     public void repair(final Ontology ontology) {
-        final var toRemove = Utils.randomChoice(ontology.minimalCorrectionSubsets(isRepaired));
+        final var toRemove = Utils.randomChoice(computeMcs(ontology));
         ontology.removeAxioms(toRemove);
     }
 
