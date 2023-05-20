3ed8b18c35dc6630d29b001ae2c4c80f35e8fcf5
Roland Bernard <rolli.bernard@gmail.com>
2023-05-20 17:37:23 +0200

    Revert "Started adding some parallelism to some repair methods"
    This reverts commit 768f07e955fc5ed3167da5a96a760ae48d6da0f8 because it
    is causing issues. There must still be some code that is not thread
    safe.

--- src/main/java/www/ontologyutils/repair/OntologyRepairBestMcs.java
+++ src/main/java/www/ontologyutils/repair/OntologyRepairBestMcs.java
@@ -1,7 +1,7 @@
 package www.ontologyutils.repair;
 
-import java.util.*;
-import java.util.function.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import www.ontologyutils.toolbox.*;
 
@@ -54,15 +54,18 @@ public class OntologyRepairBestMcs extends OntologyRepairRandomMcs {
     @Override
     public void repair(Ontology ontology) {
         var possibleCorrections = Utils.toList(mcsPeekInfo(true, computeMcs(ontology)));
-        var bestCorrection = possibleCorrections.parallelStream()
-                .map(correction -> {
-                    try (var copy = ontology.clone()) {
-                        copy.removeAxioms(correction);
-                        return new AbstractMap.SimpleEntry<>(correction, quality.apply(copy));
-                    }
-                })
-                .max(Comparator.comparingDouble(e -> e.getValue()))
-                .get().getKey();
+        var bestCorrection = possibleCorrections.get(0);
+        var bestQuality = Double.NEGATIVE_INFINITY;
+        for (var correction : possibleCorrections) {
+            try (var copy = ontology.clone()) {
+                copy.removeAxioms(correction);
+                var thisQuality = quality.apply(copy);
+                if (thisQuality > bestQuality) {
+                    bestCorrection = correction;
+                    bestQuality = thisQuality;
+                }
+            }
+        }
         ontology.removeAxioms(bestCorrection);
         infoMessage("Selected a repair with " + ontology.axioms().count() + " axioms.");
     }
